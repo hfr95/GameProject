613687d8b9581c7bd04632b2a43eb3394eb227b5
hfr95 <hfr1995@aol.com>
2015-03-30 13:50:01 -0300

    Signed-off-by: hfr95 <hfr1995@aol.com>

---
+++ TaskChallenge/Assets/PlayerMovement.js
@@ -0,0 +1,84 @@
+﻿#pragma strict
+var up : KeyCode;//Relaciona uma tecla fisica a variavel utilizando como entrada de comando
+var down : KeyCode;
+var speed : float = 2;//Determina a velocidade de deslocamento do objeto durante sua movimentaçao
+var ninja : GameObject;//Invoca o objeto player para manuseio interno pelo codigo
+var robe : GameObject;
+var animator : Animator;
+var animRobe : Animator;
+var yAx : float;
+var xAx : float;
+function Start () {
+    
+}
+
+function Update () {
+         //Bloco de movimentaçao de configuraçao de teclado padrao
+        /* yAx = ninja.transform.position.y;
+         if(Input.GetKey(up)){
+           rigidbody2D.velocity.y=speed;
+           animator.SetFloat("Speed",speed);
+           animRobe.SetFloat("Speed",speed);
+           
+           xAx = ninja.transform.position.x;
+           xAx-=0.02;
+           yAx-=0.079;
+           
+         }
+         else if(Input.GetKey(down))
+           rigidbody2D.velocity.y=speed*-1;
+         else{  
+           rigidbody2D.velocity.y=0;
+           animator.SetFloat("Speed",0);    
+           animRobe.SetFloat("Speed",0);
+           //Mantem o item Robe na mesma posiçao do personagem durante o repouso
+           xAx=-0.186;
+           yAx=yAx-0.0002;
+         }  
+         robe.transform.position.y=yAx;
+         robe.transform.position.x=xAx;
+      */ 
+    //Bloco experimental de controle de toque sensivel do sistema android
+    //Apresenta falhas       
+    yAx = ninja.transform.position.y;
+    if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+		    
+			// Get movement of the finger since last frame
+			var touchDeltaPosition:Vector2 = Input.GetTouch(0).deltaPosition;
+			if(touchDeltaPosition.y>0){
+			   animator.SetFloat("Speed",speed);
+               animRobe.SetFloat("Speed",speed);
+               xAx = ninja.transform.position.x;
+               xAx-=0.02;
+               yAx-=0.079;
+               GetComponent.<Rigidbody2D>().velocity.y=speed;
+			}
+			else{
+			    GetComponent.<Rigidbody2D>().velocity.y=speed*-1;
+			}
+			    
+                //Mantem o item Robe na mesma posiçao do personagem durante o repouso
+                
+		
+			// Move object across XY plane
+			
+			//transform.Translate (0,touchDeltaPosition.y*0.2, 0);
+	}
+	else{
+         GetComponent.<Rigidbody2D>().velocity.y=0;
+         xAx=-0.186;
+         yAx=yAx-0.0002; 
+         animator.SetFloat("Speed",0);    
+         animRobe.SetFloat("Speed",0);
+         
+     }
+     robe.transform.position.y=yAx;
+     robe.transform.position.x=xAx;
+}
+function OnCollisionEnter2D(coll: Collision2D){//Funçao nativa da coleçao unity que testa eventos de colisao dos objetos descritos
+         if(coll.gameObject.tag == "Shuriken"){
+            Destroy(ninja);
+            Application.LoadLevel("GameOver");
+         }
+         
+}
---
+++ TaskChallenge/Assets/Spawn.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var shuriken : GameObject;//Invoca o objecto shuriken
+var y : float;//Variavel de posiçao
+var quant : int = 0;//Contador de execuçoes para o instanciamento de uma nova shuriken
+
+
+function Start () {
+          
+
+}
+
+function Update () {
+        if (quant==20){//Condiçao que mantem um intervalo entre os instanciamentos
+            
+            y=Random.Range(-6.85,7.02);//Gera valores randomicos para y de acordo com os valores possiveis do tamanho do mapa
+            Instantiate(shuriken,Vector3(9.59,y,0),Quaternion.identity);//Instancia uma shuriken de acordo com os dados de posiçao
+            quant=0;//Zera contador para reiniciar o processo
+        }
+        else
+            quant++;    //Adiciona ao contador avançando a cada execuçao
+}
